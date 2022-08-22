beea8c699be650ed2a9a10627356ab5a039797fd
Rafael Rodriguez Casanova <rafaelrodriguezcasanova@proton.me>
2022-08-22 19:16:32 -0400

    woo

---
+++ CombiningArrays/woo1.js
@@ -0,0 +1,14 @@
+let testArrayOne = ["Hello","!","Name","John","!"];
+let testArrayTwo = ["World","My","Is","Adams","?"];
+
+function combineTwoArrays(arr1,arr2){
+    let combinedArray= [];
+    for (let i =0; i<arr1.length;i++){
+        combinedArray.push(arr1[i]);
+        combinedArray.push(arr2[i])
+    }   return combinedArray;
+
+}
+
+
+console.log(combineTwoArrays(testArrayOne,testArrayTwo))
